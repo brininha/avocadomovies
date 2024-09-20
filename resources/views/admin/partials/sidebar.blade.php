f55b283b09e04ed1bdf9fa7a83765ce745e6af83
Sabrina Cristan <sabrina.cristan@gmail.com>
2024-09-20 13:31:37 -0300

    Partial do menu lateral da dashboard

---
+++ resources/views/admin/partials/sidebar.blade.php
@@ -0,0 +1,10 @@
+<div class="sidebar">
+    <h2>Opções</h2>
+    <ul>
+        <li><a href="{{ url('/admin') }}">Usuários</a></li>
+        <li><a href="{{ url('/admin') }}">Filmes</a></li>
+        <li><a href="{{ url('/admin') }}">Gêneros</a></li>
+        <li><a href="{{ url('/admin') }}">Contatos</a></li>
+        <li><a href="{{ url('/') }}">Sair</a></li>
+    </ul>
+</div>
