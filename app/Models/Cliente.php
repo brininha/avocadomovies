4846c455ea9bbcfa2864b895f86ebe469d2b6b87
Sabrina Cristan <sabrina.cristan@gmail.com>
2024-09-20 19:55:12 -0300

    Ajeitando model do cliente

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Cliente extends Model
+class Cliente extends Authenticatable
 {
     use HasFactory;
 
@@ -19,4 +20,9 @@ class Cliente extends Model
     ];
 
     public $timestamps = false;
+
+    public function getAuthPassword()
+    {
+        return $this->senhaCliente;
+    }
 }
