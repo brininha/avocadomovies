49b0cc635e77f064c8a4b4a2322922423c8c7d3e
Sabrina Cristan <sabrina.cristan@gmail.com>
2024-10-11 20:58:53 -0300

    Organizando rotas da sidebar

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -1,11 +1,13 @@
 <div class="sidebar">
     <img class="logo" src="{{ asset('images/logo-dark.png') }}">
     <ul>
-        <li><i class="left-menu-icon fas fa-home"></i><a href="{{ url('/admin') }}">Home</a></li>
+        <li><i class="left-menu-icon fas fa-home"></i><a href="{{ url('/admin?view=adminHome') }}">Home</a></li>
         <li><i class="left-menu-icon fas fa-user"></i><a href="{{ url('/admin/usuarios') }}">Usuários</a></li>
         <li><i class="left-menu-icon fas fa-film"></i><a href="{{ url('/admin/filmes?view=adminFilmes') }}">Filmes</a></li>
         <li><i class="left-menu-icon fas fa-book"></i><a href="{{ url('/admin/generos') }}">Gêneros</a></li>
         <li><i class="left-menu-icon fas fa-envelope"></i><a href="{{ url('/admin') }}">Contatos</a></li>
+        <li><i class="left-menu-icon fas fa-tv"></i><a href="{{ url('/admin') }}">Sessões</a></li>
+        <li><i class="left-menu-icon fas fa-ticket-alt"></i><a href="{{ url('/admin') }}">Ingressos</a></li>
         <li><i class="left-menu-icon fas fa-door-open"></i><a href="{{ url('/') }}">Sair</a></li>
     </ul>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -21,17 +21,21 @@ Route::get('/sobre', function () {
     return view ('sobre');
 });
 
-Route::get('/admin', [ClienteController::class, 'read']);
-
 Route::put('/deletar-usuario/{id}', [ClienteController::class, 'destroy'])->name('usuarios.deletar');
 
 Route::put('/deletar-filme/{id}', [FilmeController::class, 'destroy'])->name('filmes.deletar');
 
 Route::put('/deletar-genero/{id}', [GeneroController::class, 'destroy'])->name('generos.deletar');
 
+Route::put('editar-filme/{id}', [FilmeController::class, 'update']);
+
 Route::get('/', [FilmeController::class, 'read']);
 
 Route::get('/filme/{id}', [FilmeController::class, 'find']);
+
+Route::get('/admin', [ClienteController::class, 'read']);
+
+Route::get('/admin/usuarios', [ClienteController::class, 'read']);
 
 Route::get('/admin/filmes', [FilmeController::class, 'read']);
 
@@ -44,5 +48,7 @@ Route::post('/enviar-contato', [ContatoController::class, 'store']);
 Route::post('/cadastrar-usuario', [ClienteController::class, 'store']);
 
 Route::post('/adicionar-genero', [GeneroController::class, 'store']);
+
+Route::post('/adicionar-filme', [FilmeController::class, 'store']);
 
 Route::post('/login', [ClienteController::class, 'login']);
