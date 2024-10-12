c31853c5d17579116e5ecf5646ca03558feff0e1
Sabrina Cristan <sabrina.cristan@gmail.com>
2024-10-11 21:00:13 -0300

    Colocando gráfico de colunas na dashboard

---
+++ public/js/columnChart.js
@@ -0,0 +1,44 @@
+google.charts.load('current', {packages: ['corechart', 'bar']});
+google.charts.setOnLoadCallback(drawDualY);
+
+function drawDualY() {
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Meses do ano');
+      data.addColumn('number', 'Unidades vendidas');
+
+      data.addRows([
+        ['Jan', 1],
+        ['Fev', 2],
+        ['Mar', 3],
+        ['Abr', 4],
+        ['Mai', 5],
+        ['Jun', 6],
+        ['Jul', 7],
+        ['Ago', 8],
+        ['Set', 9],
+        ['Out', 10],
+        ['Nov', 0],
+        ['Dez', 0],
+      ]);
+
+      var options = {
+        chart: {
+          title: 'Vendas de ingressos no último ano',
+        },
+        colors: '#3a6213',
+        series: {
+          0: {axis: 'unitsSold'},
+        },
+        axes: {
+          y: {
+            unitsSold: {label: 'Unidades vendidas'}
+          }
+        },
+        hAxis: {
+          title: 'Meses do ano',
+        },
+      };
+
+      var materialChart = new google.charts.Bar(document.getElementById('chart_div'));
+      materialChart.draw(data, options);
+    }
---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Avocado | Dashboard</title>
+  <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/modal.css') }}">
+  <link rel="stylesheet"
+        href="{{ url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css') }}">
+</head>
+
+<body>
+  <div class="dashboard">
+    @include('admin.partials.sidebar')
+    <div class="main-content">
+      @include('admin.partials.stats')
+      <div id="chart_div"></div>
+    </div>
+  </div>
+
+  @if (session('message'))
+      <div id="modalAlert" class="modal">
+          <div class="modal-content">
+              <p>{{ session('message') }}</p>
+              <span class="fecharMensagem">&times;</span>
+          </div>
+      </div>
+  @endif
+  </div>
+
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+  <script src="{{ asset('js/columnChart.js') }}"></script>
+  <script src="{{ asset('js/modalAlert.js') }}"></script>
+  <script src="{{ asset('js/modalExclusao.js') }}"></script>
+</body>
+
+</html>
